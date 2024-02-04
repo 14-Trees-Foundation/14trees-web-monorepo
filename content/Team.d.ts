076962be2d2cda23cb8ad12529dad219142c0cf5
Chaitanya Bhagwat <chai.bhagwat@gmail.com>
2024-02-05 01:49:19 +0530

    Add .d.ts from content to git

---
+++ content/Team.d.ts
@@ -0,0 +1,34 @@
+export interface undefinedRow {
+    howDidYouGetEngagedWith14trees: string;
+    phone: string;
+    linkedin: string;
+    myInterestshobbies: any[];
+    tenure: string;
+    engagedIn: any[];
+    startDate: string;
+    user: {
+        object: string;
+        id: string;
+        type: string;
+        name: string;
+        avatarurl: string;
+        person: {};
+        personemail: string;
+    }[];
+    volunteeringFor: any[];
+    favouriteFruit: string;
+    aboutMe: string;
+    tshirtSize: string;
+    email: string;
+    status: string;
+    languages: any[];
+    picture: {
+        type: string;
+        file: {
+            url: string;
+            expirytime: string;
+        };
+    }[];
+    dashboard: string;
+    name: string;
+}
