436b6f5aca43110656dcd47fd9797324d59edd6f
Chaitanya Bhagwat <chai.bhagwat@gmail.com>
2024-03-24 23:23:58 +0530

    Policies Links

--- apps/front-page/app/policies/page.tsx
+++ apps/front-page/app/policies/page.tsx
@@ -1,16 +1,21 @@
 import ExternalLink from "components/ExternalLink";
+import labels from "~/assets/labels.json";
 
 export default function Page() {
   return (
     <div className="container min-h-screen max-w-screen-sm bg-white py-32">
       <h1 className="title-text mb-24 text-center">Policies</h1>
       <ul className="list-none">
-        <li className="my-2">
-          <ExternalLink href="https://14trees.notion.site/14-Trees-Foundation-Policies-11fffaa9fd5f4a249dab45d3a3172951">
-            Financial and Legal
-          </ExternalLink>
-        </li>
-        <li className="my-2">
+        { 
+          labels.policies.map(({name, link}) => (
+            <li key={name}>
+              <ExternalLink href={link}>
+                {name}
+              </ExternalLink>
+            </li>
+          ))
+        }
+        {/* <li className="my-2">
           <ExternalLink href="https://14trees.notion.site/14-Trees-Foundation-Policies-11fffaa9fd5f4a249dab45d3a3172951">
             Anti-Fraud
           </ExternalLink>
@@ -29,7 +34,7 @@ export default function Page() {
           <ExternalLink href="https://14trees.notion.site/14-Trees-Foundation-Policies-11fffaa9fd5f4a249dab45d3a3172951">
             Child Protection
           </ExternalLink>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
