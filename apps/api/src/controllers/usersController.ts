d323eccab46fb6a52582042fb1c2dd8764abf755
Shivam Rathaur <shivamrathaur@Shivam-Rathaur.local>
2024-05-19 01:42:47 +0530

    changed to ts

--- apps/api/src/controllers/usersController.ts
+++ apps/api/src/controllers/usersController.ts
@@ -1,221 +1,226 @@
-// import { constants } from "../constants";
-// import { errorMessage, successMessage, status } from "../helpers/status";
-// import { UserModel, User }  from "../models/user"; // Assuming User model is defined here
-// import { UserTreeModel } from "../models/userprofile";
-// import { getUserDocumentFromRequestBody } from "./helper/users";
-// import * as csvParser from 'csv-parser'; // Import all members from csv-parser
-// import { createObjectCsvWriter } from 'csv-writer'; // Import specific function
-// import * as fs from 'fs'; // Import all members from fs
-// import { getOffsetAndLimitFromRequest } from "./helper/request";
-// /*
-//     Model - User
-//     CRUD Operations for users collection
-// */
+import { Request, Response } from "express";
+import { constants } from "../constants";
+import { errorMessage, successMessage, status } from "../helpers/status";
+import UserModel from "../models/user";
+import UserTreeModel from "../models/userprofile";
+import userHelper from "./helper/users";
+import { getOffsetAndLimitFromRequest } from "./helper/request";
+import csvParser from 'csv-parser';
+import { createObjectCsvWriter } from 'csv-writer';
+import fs from 'fs';
 
-// module.exports.addUser = async (req: any,res: any) => {
-//   try {
-//     if (!req.body?.name) {
-//       throw new Error("User name is required");
-//     }
-//   } catch (error: any) {
-//     res.status(status.bad).send({ error: error.message });
-//     return;
-//   }
+/*
+    Model - User
+    CRUD Operations for users collection
+*/
 
-//   try {
-//     let user = userHelper.getUserDocumentFromRequestBody(req.body);
-//     let result = await user.save();
-//     res.status(status.created).json(result);
-//   } catch (error: any) {
-//     res.status(status.duplicate).json({
-//       status: status.duplicate,
-//       message: error.message,
-//     });
-//   }
-// };
+export const addUser = async (req: Request, res: Response) => {
+  try {
+    if (!req.body.name) {
+      throw new Error("User name is required");
+    }
+  } catch (error) {
+    res.status(status.bad).send({ error: error.message });
+    return;
+  }
 
-// module.exports.addUsersBulk = async (req: any,res: any) => {
+  try {
+    let user = userHelper.getUserDocumentFromRequestBody(req.body);
+    let result = await user.save();
+    res.status(status.created).json(result);
+  } catch (error) {
+    res.status(status.duplicate).json({
+      status: status.duplicate,
+      message: error.message,
+    });
+  }
+};
 
-//   try {
-//     if (!req.file) {
-//       throw new Error('No file uploaded. Bulk operation requires data as csv file.');
-//     }
+export const addUsersBulk = async (req: Request, res: Response) => {
 
-//     let csvData = [];
-//     let failedRows = [];
-//     fs.createReadStream(constants.DEST_FOLDER + req.file.filename)
-//       .pipe(csvParser())
-//       .on('data', (row) => {
-//         csvData.push(row);
-//       })
-//       .on('end', async () => {
-//         try {
-//           if (csvData.length > constants.MAX_BULK_ADD_LIMIT) {
-//             throw new Error("Number of rows in csv file are more than allowed limit.")
-//           }
+  try {
+    if (!req.file) {
+      throw new Error('No file uploaded. Bulk operation requires data as csv file.');
+    }
 
-//           let users = [];
-//           let batchRows = [];
-//           for (const row of csvData) {
-//             batchRows.push(row);
-//             let user = userHelper.getUserDocumentFromRequestBody(row);
-//             users.push(user);
-//             if (users.length === constants.ADD_DB_BATCH_SIZE) {
-//               try {
-//                 await UserModel.bulkSave(users);
-//               } catch (error: any) {
-//                 failedRows.push(...batchRows.map(row => ({ ...row, success: false, error: error.message })));
-//               }
-//               batchRows = [];
-//               users = [];
-//             }
-//           }
+    let csvData = [];
+    let failedRows = [];
+    fs.createReadStream(constants.DEST_FOLDER + req.file.filename)
+      .pipe(csvParser())
+      .on('data', (row) => {
+        csvData.push(row);
+      })
+      .on('end', async () => {
+        try {
+          if (csvData.length > constants.MAX_BULK_ADD_LIMIT) {
+            throw new Error("Number of rows in csv file are more than allowed limit.")
+          }
 
-//           if (users.length !== 0) {
-//             try {
-//               await UserModel.bulkSave(users);
-//             } catch (error: any) {
-//               failedRows.push(...batchRows.map(row => ({ ...row, success: false, error: error.message })));
-//             }
-//           }
+          let users = [];
+          let batchRows = [];
+          for (const row of csvData) {
+            batchRows.push(row);
+            let user = userHelper.getUserDocumentFromRequestBody(row);
+            users.push(user);
+            if (users.length === constants.ADD_DB_BATCH_SIZE) {
+              try {
+                await UserModel.bulkSave(users);
+              } catch (error) {
+                failedRows.push(...batchRows.map(row => ({ ...row, success: false, error: error.message })));
+              }
+              batchRows = [];
+              users = [];
+            }
+          }
 
-//           // Prepare the response
-//           let responseCsv = ''
-//           const filePath = constants.DEST_FOLDER + Date.now().toString() + '_' + 'failed_user_records.csv'
-//           if (failedRows.length > 0) {
-//             // Generate CSV string for failed rows
-//             const csvWriter = createCsvWriter({
-//               path: filePath,
-//               header: Object.keys(failedRows[0]).map(key => ({ id: key, title: key }))
-//             });
-//             await csvWriter.writeRecords(failedRows);
-//             responseCsv = fs.readFileSync(filePath);
-//           }
+          if (users.length !== 0) {
+            try {
+              await UserModel.bulkSave(users);
+            } catch (error) {
+              failedRows.push(...batchRows.map(row => ({ ...row, success: false, error: error.message })));
+            }
+          }
 
-//           // Send the response with CSV content
-//           res.setHeader('Content-Disposition', 'attachment; filename="failed_rows.csv"');
-//           res.setHeader('Content-Type', 'text/csv');
-//           res.send(responseCsv);
-//         } catch (error: any) {
-//           console.error('Error saving User bulk data:', error);
-//           res.status(500).json({ error: 'Error saving users data.' });
-//         }
-//       });
+          // Prepare the response
+          let responseCsv = ''
+          const filePath = constants.DEST_FOLDER + Date.now().toString() + '_' + 'failed_user_records.csv'
+          if (failedRows.length > 0) {
+            // Generate CSV string for failed rows
+            const csvWriter = createObjectCsvWriter({
+              path: filePath,
+              header: Object.keys(failedRows[0]).map(key => ({ id: key, title: key }))
+            });
+            await csvWriter.writeRecords(failedRows);
+            responseCsv = fs.readFileSync(filePath);
+          }
 
-//   } catch (error: any) {
-//     console.error('Error processing CSV:', error);
-//     res.status(500).json({ error: error.message });
-//   }
-// };
+          // Send the response with CSV content
+          res.setHeader('Content-Disposition', 'attachment; filename="failed_rows.csv"');
+          res.setHeader('Content-Type', 'text/csv');
+          res.send(responseCsv);
+        } catch (error) {
+          console.error('Error saving User bulk data:', error);
+          res.status(500).json({ error: 'Error saving users data.' });
+        }
+      });
 
-// module.exports.getUser = async (req: any,res: any) => {
-//   try {
-//     if (req.query.email && req.query.name) {
-//       let userid = req.query.name.toLowerCase() + req.query.email.toLowerCase();
-//       userid = userid.replace(/[^A-Z0-9@.]+/gi, "");
+  } catch (error) {
+    console.error('Error processing CSV:', error);
+    res.status(500).json({ error: error.message });
+  }
+};
 
-//       let result = await UserModel.findOne({ userid: userid });
-//       if (result === null) {
-//         res.status(status.notfound).send();
-//       } else {
-//         let lastProfile = await UserTreeModel.find(
-//           { user: result._id },
-//           { _id: 0 },
-//         )
-//           .populate({
-//             path: "tree",
-//             select: "sapling_id date_added -_id",
-//             populate: { path: "tree_id", select: "name -_id" },
-//           })
-//           .select("tree");
-//         res.status(status.success).json({
-//           user: result,
-//           tree: lastProfile,
-//         });
-//       }
-//     } else {
-//       const { offset, limit } = getOffsetAndLimitFromRequest(req);
-//       let result = await UserModel.find().skip(offset).limit(limit);
-//       res.status(status.success).json(result);
-//     }
+export const getUser = async (req: Request, res: Response) => {
+  try {
+    if (req.query.email && req.query.name) {
+      let userid = req.query.name.toLowerCase() + req.query.email.toLowerCase();
+      userid = userid.replace(/[^A-Z0-9@.]+/gi, "");
 
-//   } catch (error: any) {
-//     res.status(status.bad).send({ error: error.message });
-//     return;
-//   }
-// };
+      let result = await UserModel.findOne({ userid: userid });
+      if (result === null) {
+        res.status(status.notfound).send();
+      } else {
+        let lastProfile = await UserTreeModel.find(
+          { user: result._id },
+          { _id: 0 },
+        )
+          .populate({
+            path: "tree",
+            select: "sapling_id date_added -_id",
+            populate: { path: "tree_id", select: "name -_id" },
+          })
+          .select("tree");
+        res.status(status.success).json({
+          user: result,
+          tree: lastProfile,
+        });
+      }
+    } else {
+      const { offset, limit } = getOffsetAndLimitFromRequest(req);
+      let result = await UserModel.find().skip(offset).limit(limit);
+      res.status(status.success).json(result);
+    }
 
-// module.exports.getUsersByEmailIdPrefix = async (req: any,res: any) => {
-//   try {
-//     if (!req.params.email || req.params.email.length < 3) {
-//       res.status(status.bad).send({ error: "Please provide at least 3 char of email"});
-//       return;
-//     }
+  } catch (error) {
+    res.status(status.bad).send({ error: error.message });
+    return;
+  }
+};
 
-//     const { offset, limit } = getOffsetAndLimitFromRequest(req);
-//     const regex = new RegExp(req.params.email, 'i');
-//     const users = await UserModel.find({ email: { $regex: regex } }).skip(offset).limit(limit);
-//     res.status(status.success).send(users);
-//     return;
-//   } catch (error: any) {
-//     res.status(status.bad).send({ error: error.message });
-//     return;
-//   }
-// };
+export const searchUsers = async (req: Request, res: Response) => {
+  try {
+    if (!req.params.search || req.params.search.length < 3) {
+      res.status(status.bad).send({ error: "Please provide at least 3 char to search"});
+      return;
+    }
 
-// module.exports.getUsers = async (req: any,res: any) => {
-//   const { offset, limit } = getOffsetAndLimitFromRequest(req);
-//   try {
-//     let result = await UserModel.find().skip(offset).limit(limit);
-//     res.status(status.success).json(result);
-//   } catch (error: any) {
-//     res.status(status.error).json({
-//       status: status.error,
-//       message: error.message,
-//     });
-//   }
-// };
+    const { offset, limit } = getOffsetAndLimitFromRequest(req);
+    const regex = new RegExp(req.params.search, 'i');
+    const users = await UserModel.find({
+      $or: [
+        {email: { $regex: regex }},
+        {name: { $regex: regex }},
+      ]
+    }).skip(offset).limit(limit);
+    res.status(status.success).send(users);
+    return;
+  } catch (error) {
+    res.status(status.bad).send({ error: error.message });
+    return;
+  }
+};
 
-// module.exports.updateUser = async (req: any,res: any) => {
+export const getUsers = async (req: Request, res: Response) => {
+  const { offset, limit } = getOffsetAndLimitFromRequest(req);
+  try {
+    let result = await UserModel.find().skip(offset).limit(limit);
+    res.status(status.success).json(result);
+  } catch (error) {
+    res.status(status.error).json({
+      status: status.error,
+      message: error.message,
+    });
+  }
+};
 
-//   try {
-//     let user = await UserModel.findById(req.params.id);
-//     if (!user) {
-//       res.status(status.notfound).json({
-//         status: status.notfound,
-//         message: "User not found with given id"
-//       })
-//     }
+export const updateUser = async (req: Request, res: Response) => {
 
-//     if (req.body.name) user.name = req.body.name;
-//     if (req.body.email) user.email = req.body.email;
-//     if (req.body.dob) user.dob = req.body.dob;
-//     if (req.body.phone) user.phone = req.body.phone;
-//     if (req.body.org) user.org = req.body.org;
+  try {
+    let user = await UserModel.findById(req.params.id);
+    if (!user) {
+      res.status(status.notfound).json({
+        status: status.notfound,
+        message: "User not found with given id"
+      })
+    }
 
-//     user.userid = userHelper.getUserId(user.name, user.email);
+    if (req.body.name) user.name = req.body.name;
+    if (req.body.email) user.email = req.body.email;
+    if (req.body.dob) user.dob = req.body.dob;
+    if (req.body.phone) user.phone = req.body.phone;
+    if (req.body.org) user.org = req.body.org;
 
-//     let result = await user.save();
-//     res.status(status.success).json(result);
-//   } catch (error: any) {
-//     res.status(status.error).json({
-//       status: status.error,
-//       message: error.message,
-//     });
-//   }
-// };
+    user.userid = userHelper.getUserId(user.name, user.email);
 
+    let result = await user.save();
+    res.status(status.success).json(result);
+  } catch (error) {
+    res.status(status.error).json({
+      status: status.error,
+      message: error.message,
+    });
+  }
+};
 
-// module.exports.deleteUser = async (req: any,res: any) => {
-
-//   try {
-//     let resp = await UserModel.findByIdAndDelete(req.params.id).exec();
-//     console.log("Deleted User with id: %s", req.params.id, resp)
-//     res.status(status.success).json(resp);
-//   } catch (error: any) {
-//     res.status(status.error).json({
-//       status: status.error,
-//       message: error.message,
-//     });
-//   }
-// };
+export const deleteUser = async (req: Request, res: Response) => {
+    try {
+      let resp = await UserModel.findByIdAndDelete(req.params.id).exec();
+      console.log(`Deleted User with id: ${req.params.id}`, resp);
+      res.status(status.success).json(resp);
+    } catch (error) {
+      res.status(status.error).json({
+        status: status.error,
+        message: error.message,
+      });
+    }
+  };
--- apps/api/src/routes/treeRoutes.ts
+++ apps/api/src/routes/treeRoutes.ts
@@ -1,38 +1,38 @@
-// import { Router } from "express";
-// import * as trees from "../controllers/treesController";
-// import uploadImages from "../helpers/multer";
+import { Router } from "express";
+import * as trees from "../controllers/treesController";
+import uploadImages from "../helpers/multer";
 
-// const routes = Router();
+const routes = Router();
 
-// // TreeTypes
-// routes.get("/treetypes", trees.getTreeTypes);
-// routes.post("/addtreetype", uploadImages.array("files", 4), trees.addTreeType);
-// routes.put('/treetypes/:id', uploadImages.array('files', 4), trees.updateTreeType);
-// routes.delete('/treetypes/:id', trees.deleteTreeType);
+// TreeTypes
+routes.get("/treetypes", trees.getTreeTypes);
+routes.post("/addtreetype", uploadImages.array("files", 4), trees.addTreeType);
+routes.put('/treetypes/:id', uploadImages.array('files', 4), trees.updateTreeType);
+routes.delete('/treetypes/:id', trees.deleteTreeType);
 
-// // Trees
+// Trees
 
-// routes.get('/gettree', trees.getTree);
-// // @deprecated
-// routes.post('/addtree', uploadImages.array('files', 1), trees.addTree);
+routes.get('/gettree', trees.getTree);
+// @deprecated
+routes.post('/addtree', uploadImages.array('files', 1), trees.addTree);
 
-// // routes.get('/:sapling_id', trees.getTree);
-// routes.get('/', trees.getTrees);
-// routes.post('/', uploadImages.array('files', 1), trees.addTree);
-// routes.post('/bulk', uploadImages.fields([{name: 'files', maxCount: 1}, {name: 'csvFile', maxCount: 1}]), trees.addTreesBulk);
-// routes.put('/:id', uploadImages.array('files', 1), trees.updateTree);
-// routes.delete('/:id', uploadImages.array('files', 1), trees.deleteTree);
+// routes.get('/:sapling_id', trees.getTree);
+routes.get('/', trees.getTrees);
+routes.post('/', uploadImages.array('files', 1), trees.addTree);
+routes.post('/bulk', uploadImages.fields([{name: 'files', maxCount: 1}, {name: 'csvFile', maxCount: 1}]), trees.addTreesBulk);
+routes.put('/:id', uploadImages.array('files', 1), trees.updateTree);
+routes.delete('/:id', uploadImages.array('files', 1), trees.deleteTree);
 
 
-// routes.get("/getsaplingid", trees.getTreeFromId);
-// routes.get("/groupbyplots", trees.treeCountByPlot);
-// routes.get("/loggedbydate", trees.treeLoggedByDate);
-// routes.get("/treelogbyuser", trees.treeLogByUser);
-// routes.get("/treelogbyplot", trees.treeLogByPlot);
-// routes.get("/treetypecount", trees.treeCountTreeType);
-// routes.get("/treetypecount/plotwise", trees.treeTypeCountByPlot);
-// routes.get("/plot/count", trees.countByPlot);
-// routes.get("/plot/list", trees.treeListByPlot);
-// routes.post('/update/photo', uploadImages.array('files', 1), trees.addPhotoUpdate);
+routes.get("/getsaplingid", trees.getTreeFromId);
+routes.get("/groupbyplots", trees.treeCountByPlot);
+routes.get("/loggedbydate", trees.treeLoggedByDate);
+routes.get("/treelogbyuser", trees.treeLogByUser);
+routes.get("/treelogbyplot", trees.treeLogByPlot);
+routes.get("/treetypecount", trees.treeCountTreeType);
+routes.get("/treetypecount/plotwise", trees.treeTypeCountByPlot);
+routes.get("/plot/count", trees.countByPlot);
+routes.get("/plot/list", trees.treeListByPlot);
+routes.post('/update/photo', uploadImages.array('files', 1), trees.addPhotoUpdate);
 
-// export default routes;
+export default routes;
