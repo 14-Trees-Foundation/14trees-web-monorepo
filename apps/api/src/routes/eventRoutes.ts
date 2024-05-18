ea88cfe1dae23568ed8c67de95b23880ed0e01fa
Shivam Rathaur <shivamrathaur@Shivam-Rathaur.local>
2024-05-19 00:04:39 +0530

    changed to ts

--- apps/api/src/routes/eventRoutes.ts
+++ apps/api/src/routes/eventRoutes.ts
@@ -1,21 +1,21 @@
-// import { Router } from "express";
-// import * as events from "../controllers/eventsController";
-// import uploadImages from "../helpers/multer";
+import { Router } from "express";
+import * as events from "../controllers/eventsController";
+import uploadImages from "../helpers/multer";
 
-// const routes = Router();
+const routes = Router();
 
-// // @deprecated
-// routes.post('/addevents', uploadImages.array('files',1), events.addEvents);
+// @deprecated
+routes.post('/addevents', uploadImages.array('files',1), events.addEvents);
 
-// routes.get('/', events.getEvents);
-// routes.post('/', uploadImages.array('files', 1), events.addEvents);
-// routes.delete('/:id', events.deleteEvent);
-// routes.get("/birthday", events.getBirthdayEvent);
-// routes.get("/org", events.getOverallOrgDashboard);
-// routes.get("/plot", events.getOverallPlotDashboard);
-// routes.post("/corp/add", uploadImages.array("files", 12), events.addCorpEvent);
-// routes.get("/corp/", events.getCorpEvent);
-// routes.put('/corp/:id', events.updateCorpEvent);
-// routes.delete('/corp/:id', events.deleteCorpEvent);
+routes.get('/', events.getEvents);
+routes.post('/', uploadImages.array('files', 1), events.addEvents);
+routes.delete('/:id', events.deleteEvent);
+routes.get("/birthday", events.getBirthdayEvent);
+routes.get("/org", events.getOverallOrgDashboard);
+routes.get("/plot", events.getOverallPlotDashboard);
+routes.post("/corp/add", uploadImages.array("files", 12), events.addCorpEvent);
+routes.get("/corp/", events.getCorpEvent);
+routes.put('/corp/:id', events.updateCorpEvent);
+routes.delete('/corp/:id', events.deleteCorpEvent);
 
-// export default routes;
+export default routes;
