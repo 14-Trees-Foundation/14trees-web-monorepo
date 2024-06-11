cb489a71ff9b74b0bde44ae7a560a4364e10d476
Dhruvin-Moradiya <dsmoradiya22@gmail.com>
2024-06-11 17:59:46 +0530

    updated postman collection

--- apps/api/src/swagger.ts
+++ apps/api/src/swagger.ts
@@ -4,7 +4,7 @@ const outputFile = './swagger_output.json'
 const routes = ['src/server.ts'];
 const doc = {
     info: {
-      title: '14Trees-API',
+      title: '14Trees-Postgres-API',
       description: 'Backend APIs for 14Trees-service'
     },
     host: 'localhost:8088'
