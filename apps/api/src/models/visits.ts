d06d153deed08b06729e40a96dc59782a230dcf5
Dhruvin-Moradiya <dsmoradiya22@gmail.com>
2024-09-10 16:52:56 +0530

    added group id mapping in visit table

--- apps/api/src/models/visits.ts
+++ apps/api/src/models/visits.ts
@@ -9,43 +9,47 @@ interface VisitAttributes {
   site_id: number;
   created_at: Date;
   updated_at: Date;
+  group_id: number | null;
 }
 
 
-interface VisitCreationAttributes 
-    extends Optional<VisitAttributes, 'id'> {}
+interface VisitCreationAttributes
+  extends Optional<VisitAttributes, 'id' |'group_id'> { }
 
- @Table({ tableName: 'visits' })
- class Visit
- extends Model<VisitAttributes, VisitCreationAttributes>
- implements VisitAttributes {
-     @Column({
-       type: DataType.NUMBER,
-       allowNull: false,
-       autoIncrement: true,
-       primaryKey: true,
-       unique: true
-     })
-     id!: number;
- 
-     @Column({ type: DataType.STRING})
-     visit_name!: string;
- 
-     @Column({ type: DataType.DATE })
-     visit_date!: Date;
+@Table({ tableName: 'visits' })
+class Visit
+  extends Model<VisitAttributes, VisitCreationAttributes>
+  implements VisitAttributes {
+  @Column({
+    type: DataType.NUMBER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    unique: true
+  })
+  id!: number;
 
-     @Column({ type: DataType.STRING })
-     visit_type!: string;
- 
-     @Column({ type: DataType.STRING })
-     site_id!: number;
+  @Column({ type: DataType.STRING })
+  visit_name!: string;
 
-     @Column({ type: DataType.DATE })
-     created_at!: Date;
+  @Column({ type: DataType.DATE })
+  visit_date!: Date;
 
-     @Column({ type: DataType.DATE })
-     updated_at!: Date;
-    }  
+  @Column({ type: DataType.STRING })
+  visit_type!: string;
 
-    export { Visit }
+  @Column({ type: DataType.NUMBER })
+  site_id!: number;
+
+  @Column({ type: DataType.NUMBER })
+  group_id!: number;
+
+  @Column({ type: DataType.DATE })
+  created_at!: Date;
+
+  @Column({ type: DataType.DATE })
+  updated_at!: Date;
+}
+
+export { Visit }
 export type { VisitAttributes, VisitCreationAttributes }
