7e8fb3dd5dcea12b0d642fb6d0af5e016894a690
Dhruvin-Moradiya <dsmoradiya22@gmail.com>
2024-10-22 18:37:59 +0530

    issue fix

--- apps/api/src/repo/giftCardsRepo.ts
+++ apps/api/src/repo/giftCardsRepo.ts
@@ -311,7 +311,7 @@ export class GiftCardsRepository {
             LEFT JOIN "14trees_2".trees t ON t.id = gc.tree_id
             LEFT JOIN "14trees_2".plant_types pt ON pt.id = t.plant_type_id
             LEFT JOIN "14trees_2".plots p ON p.id = t.plot_id
-            LEFT JOIN "14trees".sites p ON s.id = p.site_id
+            LEFT JOIN "14trees".sites s ON s.id = p.site_id
             WHERE gc.gift_card_request_id = ${giftCardRequestId}
             ORDER BY gc.id
         `
