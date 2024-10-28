b45349af98b89a75958a57f21e15ef5ef121a4b4
Dhruvin-Moradiya <dsmoradiya22@gmail.com>
2024-10-28 10:19:55 +0530

    plot live aggregations

--- apps/api/src/repo/plotRepo.ts
+++ apps/api/src/repo/plotRepo.ts
@@ -77,19 +77,81 @@ export class PlotRepository {
                     then s.name_marathi
                     else s.name_english 
                 end site_name,
-                SUM(coalesce(tcg.booked, 0)) as booked,
-                SUM(coalesce(tcg.available, 0)) as available,
-                SUM(coalesce(tcg.assigned, 0)) as assigned,
-                SUM(coalesce(tcg.total, 0)) as total,
-                SUM(coalesce(tcg.void_total, 0)) as void_total,
-                SUM(coalesce(tcg.void_booked, 0)) as void_booked,
-                SUM(coalesce(tcg.void_available, 0)) as void_available,
-                SUM(coalesce(tcg.void_assigned, 0)) as void_assigned,
-                SUM(coalesce(tcg.card_available, 0)) as card_available,
-                SUM(coalesce(tcg.unbooked_assigned, 0)) as unbooked_assigned
+            COUNT(t.id) as total, 
+            COUNT(t.assigned_to) as assigned,
+            SUM(CASE 
+                WHEN t.mapped_to_user IS NOT NULL 
+                    OR t.mapped_to_group IS NOT NULL
+                THEN 1 
+                ELSE 0 
+            END) AS booked,
+            SUM(CASE 
+                WHEN t.mapped_to_user IS NULL 
+                    AND t.mapped_to_group IS NULL 
+                    AND t.assigned_to IS NULL 
+                    AND t.id IS NOT NULL
+                THEN 1 
+                ELSE 0 
+            END) AS available,
+            SUM(case
+                WHEN ts.tree_status = 'dead'
+                    or ts.tree_status = 'lost'
+                THEN 1
+                ELSE 0
+            END) as void_total,
+            SUM(case
+                WHEN (ts.tree_status = 'dead'
+                    or ts.tree_status = 'lost') and t.assigned_to is not null
+                THEN 1
+                ELSE 0
+            END) as void_assigned,
+            SUM(CASE 
+                WHEN (t.mapped_to_user IS NOT NULL 
+                    OR t.mapped_to_group IS NOT NULL)
+                    and (ts.tree_status = 'dead'
+                    or ts.tree_status = 'lost')
+                THEN 1 
+                ELSE 0 
+            END) AS void_booked,
+            SUM(CASE 
+                WHEN (t.mapped_to_user IS NULL 
+                    AND t.mapped_to_group IS NULL 
+                    AND t.assigned_to IS NULL 
+                    AND t.id IS NOT NULL)
+                    and (ts.tree_status = 'dead'
+                    or ts.tree_status = 'lost')
+                THEN 1 
+                ELSE 0 
+            END) AS void_available,
+            SUM(CASE 
+                WHEN t.mapped_to_user IS NULL 
+                    AND t.mapped_to_group IS NULL 
+                    AND t.assigned_to IS NULL 
+                    AND t.id IS NOT NULL
+                    AND (ts.tree_status IS NULL OR (ts.tree_status != 'dead' AND ts.tree_status != 'lost'))
+                    AND ptct.plant_type IS NOT NULL
+                THEN 1 
+                ELSE 0 
+            END) AS card_available,
+            SUM(CASE 
+                WHEN t.mapped_to_user IS NULL 
+                    AND t.mapped_to_group IS NULL
+                    AND t.assigned_to IS NOT NULL
+                THEN 1 
+                ELSE 0 
+            END) AS unbooked_assigned
         FROM "14trees".plots p
         LEFT JOIN "14trees".sites s ON p.site_id = s.id
-        left join "14trees".tree_count_aggregations tcg on tcg.plot_id = p.id
+        LEFT JOIN "14trees".trees t ON t.plot_id = p.id
+        LEFT JOIN "14trees".plant_types pt on pt.id = t.plant_type_id
+        LEFT JOIN "14trees".plant_type_card_templates ptct on ptct.plant_type = pt."name"
+        LEFT JOIN (SELECT *
+            FROM (
+                SELECT *,
+                    ROW_NUMBER() OVER (PARTITION BY sapling_id ORDER BY created_at DESC) AS rn
+                FROM "14trees".trees_snapshots
+            ) AS snapshots
+        WHERE snapshots.rn = 1) as ts ON ts.sapling_id = t.sapling_id
         WHERE ${whereCondition !== "" ? whereCondition : "1=1"}
         GROUP BY p.id, s.id
         ORDER BY ${ orderBy && orderBy.length !== 0 ? orderBy.map(o => o.column + " " + o.order).join(", ") : 'p.id DESC'}
