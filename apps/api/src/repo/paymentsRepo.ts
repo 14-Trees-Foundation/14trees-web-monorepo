e8012fea9f1d3cbdc713fa32bbeb3766c737ddd0
Dhruvin-Moradiya <dsmoradiya22@gmail.com>
2024-11-15 18:35:16 +0530

    payments

--- apps/api/src/repo/paymentsRepo.ts
+++ apps/api/src/repo/paymentsRepo.ts
@@ -1,9 +1,24 @@
+import { QueryTypes } from 'sequelize';
+import { sequelize } from '../config/postgreDB';
 import { Payment , PaymentAttributes, PaymentCreationAttributes} from '../models/payment'
 
 export class PaymentRepository {
 
     public static async getPayment(id: number): Promise<Payment | null> {
-        return await Payment.findByPk(id);
+        const query = `
+            SELECT p.*, array_agg(ph.*) as payment_history
+            FROM "14trees_2".payments p
+            JOIN "14trees_2".payment_history ph ON ph.payment_id = p.id
+            WHERE p.id = :id
+            GROUP BY p.id;
+        `
+
+        const payments: any[] = await sequelize.query(query, {
+            type: QueryTypes.SELECT,
+            replacements: { id }
+        })
+
+        return payments.length > 0 ? payments[0] : null;
     }
 
     public static async createPayment(paymentData: PaymentCreationAttributes ): Promise<Payment> {
