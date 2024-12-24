fa73a5309d2292faa2e0ef31b34c50e23e8ee1ff
Dhruvin-Moradiya <dsmoradiya22@gmail.com>
2024-12-24 11:28:58 +0530

    inventory improvements

--- apps/api/src/repo/plotRepo.ts
+++ apps/api/src/repo/plotRepo.ts
@@ -156,6 +156,87 @@ export class PlotRepository {
                 THEN 1
                 ELSE 0
             END) AS herb_count,
+            SUM(CASE 
+                WHEN t.assigned_to IS NOT NULL 
+                    AND pt.habit = 'Tree'
+                THEN 1 
+                ELSE 0 
+            END) as assigned_trees,
+            SUM(CASE 
+                WHEN (t.mapped_to_user IS NOT NULL 
+                    OR t.mapped_to_group IS NOT NULL) AND pt.habit = 'Tree'
+                THEN 1 
+                ELSE 0 
+            END) AS booked_trees,
+            SUM(CASE 
+                WHEN (t.mapped_to_user IS NULL 
+                    AND t.mapped_to_group IS NULL
+                    AND t.assigned_to IS NOT NULL) AND pt.habit = 'Tree'
+                THEN 1 
+                ELSE 0 
+            END) AS unbooked_assigned_trees,
+            SUM(CASE 
+                WHEN (t.mapped_to_user IS NULL 
+                    AND t.mapped_to_group IS NULL 
+                    AND t.assigned_to IS NULL 
+                    AND t.id IS NOT NULL) AND pt.habit = 'Tree'
+                THEN 1 
+                ELSE 0 
+            END) AS available_trees,
+            SUM(CASE 
+                WHEN t.assigned_to IS NOT NULL 
+                    AND pt.habit = 'Herb'
+                THEN 1 
+                ELSE 0 
+            END) as assigned_herbs,
+            SUM(CASE 
+                WHEN (t.mapped_to_user IS NOT NULL 
+                    OR t.mapped_to_group IS NOT NULL) AND pt.habit = 'Herb'
+                THEN 1 
+                ELSE 0 
+            END) AS booked_herbs,
+            SUM(CASE 
+                WHEN (t.mapped_to_user IS NULL 
+                    AND t.mapped_to_group IS NULL
+                    AND t.assigned_to IS NOT NULL) AND pt.habit = 'Herb'
+                THEN 1 
+                ELSE 0 
+            END) AS unbooked_assigned_herbs,
+            SUM(CASE 
+                WHEN (t.mapped_to_user IS NULL 
+                    AND t.mapped_to_group IS NULL 
+                    AND t.assigned_to IS NULL 
+                    AND t.id IS NOT NULL) AND pt.habit = 'Herb'
+                THEN 1 
+                ELSE 0 
+            END) AS available_herbs,
+            SUM(CASE 
+                WHEN t.assigned_to IS NOT NULL 
+                    AND pt.habit = 'Shrub'
+                THEN 1 
+                ELSE 0 
+            END) as assigned_shrubs,
+            SUM(CASE 
+                WHEN (t.mapped_to_user IS NOT NULL 
+                    OR t.mapped_to_group IS NOT NULL) AND pt.habit = 'Shrub'
+                THEN 1 
+                ELSE 0 
+            END) AS booked_shrubs,
+            SUM(CASE 
+                WHEN (t.mapped_to_user IS NULL 
+                    AND t.mapped_to_group IS NULL
+                    AND t.assigned_to IS NOT NULL) AND pt.habit = 'Shrub'
+                THEN 1 
+                ELSE 0 
+            END) AS unbooked_assigned_shrubs,
+            SUM(CASE 
+                WHEN (t.mapped_to_user IS NULL 
+                    AND t.mapped_to_group IS NULL 
+                    AND t.assigned_to IS NULL 
+                    AND t.id IS NOT NULL) AND pt.habit = 'Shrub'
+                THEN 1 
+                ELSE 0 
+            END) AS available_shrubs,
             array_agg(distinct CASE 
                 WHEN t.mapped_to_user IS NULL 
                     AND t.mapped_to_group IS NULL 
