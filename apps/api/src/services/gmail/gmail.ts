4d8baada63baeecb278f514902fa5c8e4a7f4cf1
Dhruvin-Moradiya <dsmoradiya22@gmail.com>
2025-02-03 13:49:24 +0530

    whats app chatbot: handling morethan 5 recipients

--- apps/api/src/services/gmail/gmail.ts
+++ apps/api/src/services/gmail/gmail.ts
@@ -103,5 +103,26 @@ const sendDashboardMail = async (templateName: string, emailData: any, toEmails:
   return statusText;
 }
 
-export { sendDashboardMail };
+const sendTemplateMail = async (templateName: string, options: MailOptions, emailData: any) => {
+
+  const mailOptions = {
+    ...options,
+    from: { name: '14 Trees', address: 'admin@14trees.org' },
+    replyTo: 'admin@14trees.org',
+    html: "",
+    textEncoding: 'base64',
+  }
+
+  mailOptions.html = getHtmlTemplate(templateName, emailData)
+
+  const { status, statusText } = await sendMail(mailOptions)
+  if (status === 200) {
+    console.log("Email send Successfully to " + mailOptions.to);
+    return '';
+  }
+
+  return statusText;
+}
+
+export { sendDashboardMail, sendTemplateMail };
 export default sendMail;
