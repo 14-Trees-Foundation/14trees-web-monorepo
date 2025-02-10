21011fc58b3afff4fb275e61e1a3902ff5d878ff
Dhruvin-Moradiya <dsmoradiya22@gmail.com>
2025-02-10 18:37:31 +0530

    WA Chatbot: edit recipients & razorpay payments

--- apps/api/src/services/genai/WhatsApp/index.ts
+++ apps/api/src/services/genai/WhatsApp/index.ts
@@ -1,8 +1,9 @@
 import { StructuredToolInterface } from "@langchain/core/tools";
 import createSendImagesToUserTool from "./SendImagesToUser";
+import createSendEditRecipientsFlowTool from "./SendEditRecipientsFlow";
 
 // Function to get tools
 export function getWhatsAppTools(customerPhoneNumber: string): StructuredToolInterface[] {
 
-    return [createSendImagesToUserTool(customerPhoneNumber)];
+    return [createSendImagesToUserTool(customerPhoneNumber), createSendEditRecipientsFlowTool(customerPhoneNumber)];
 }
