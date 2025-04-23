10ee829ce212b72a802813907bd56dfab9c04111
Dhruvin-Moradiya <dsmoradiya22@gmail.com>
2025-04-23 16:07:26 +0530

    prod schema

--- apps/api/src/repo/treesSnapshotsRepo.ts
+++ apps/api/src/repo/treesSnapshotsRepo.ts
@@ -109,9 +109,9 @@ export class TreesSnapshotRepository {
                 DATE(ts.image_date) AS audit_date, 
                 u.name AS user_name, 
                 COUNT(t.sapling_id) AS total_trees_audited
-            FROM "14trees_2".trees_snapshots ts
-            JOIN "14trees_2".trees t ON ts.sapling_id = t.sapling_id
-            JOIN "14trees_2".users u ON ts.user_id = u.id
+            FROM "14trees".trees_snapshots ts
+            JOIN "14trees".trees t ON ts.sapling_id = t.sapling_id
+            JOIN "14trees".users u ON ts.user_id = u.id
             GROUP BY u.id, DATE(ts.image_date)
             ORDER BY audit_date DESC;
         `;
@@ -129,17 +129,17 @@ export class TreesSnapshotRepository {
                 p."name" AS plot_name,
                 t.sapling_id,
                 latest_ts.image_date
-            FROM "14trees_2".trees t
-            JOIN "14trees_2".plots p ON t.plot_id = p.id
+            FROM "14trees".trees t
+            JOIN "14trees".plots p ON t.plot_id = p.id
             LEFT JOIN (
                 SELECT DISTINCT ON (sapling_id) sapling_id, image_date
-                FROM "14trees_2".trees_snapshots
+                FROM "14trees".trees_snapshots
                 ORDER BY sapling_id, image_date DESC
             ) latest_ts ON latest_ts.sapling_id = t.sapling_id
             WHERE t.plot_id IN (
                 SELECT DISTINCT t.plot_id
-                FROM "14trees_2".trees t
-                JOIN "14trees_2".trees_snapshots ts ON ts.sapling_id = t.sapling_id
+                FROM "14trees".trees t
+                JOIN "14trees".trees_snapshots ts ON ts.sapling_id = t.sapling_id
             );
         `;
     
