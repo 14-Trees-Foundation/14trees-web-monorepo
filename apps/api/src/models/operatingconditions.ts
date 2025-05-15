e2298151fc9acf712c477480da1bc7ef35c9cd11
elishaa31 <elishaaa31@gmail.com>
2025-05-15 13:10:21 +0530

    modified operating conditions model

--- apps/api/src/models/operatingconditions.ts
+++ apps/api/src/models/operatingconditions.ts
@@ -4,46 +4,91 @@ import { Table, Column, Model, DataType } from 'sequelize-typescript';
 
 interface OperatingConditionsAttributes {
   imo_number: string;
-  parameter: string;
-  unit: string;
-  value: number;
+  engine_load: number;
+  engine_rpm: number;
+  shaft_power: number;
+  fuel_oil_consumption: number;
+  cylinder_lubricant_rate: number;
+  scavenge_air_pressure: number;
+  turbocharger_rpm: number;
 }
 
-interface OperatingConditionsCreationAttributes extends Optional<OperatingConditionsAttributes, never> {}
+interface OperatingConditionsCreationAttributes 
+  extends Optional<OperatingConditionsAttributes, never> {}
 
 @Table({ 
   tableName: 'operating_conditions',
   schema: 'Shipping',
   timestamps: false
 })
-class OperatingConditions extends Model<OperatingConditionsAttributes, OperatingConditionsCreationAttributes> 
-  implements OperatingConditionsAttributes {
+class OperatingConditions extends Model<
+  OperatingConditionsAttributes, 
+  OperatingConditionsCreationAttributes
+> implements OperatingConditionsAttributes {
 
   @Column({
     type: DataType.STRING(10),
     primaryKey: true,
-    allowNull: false
+    allowNull: false,
+    field: 'imo_number'
   })
   imo_number!: string;
 
   @Column({
-    type: DataType.STRING(50),
-    primaryKey: true,
-    allowNull: false
+    type: DataType.DECIMAL(5,2),
+    allowNull: false,
+    field: 'engine_load',
+    comment: 'Percentage value'
   })
-  parameter!: string;
+  engine_load!: number;
 
   @Column({
-    type: DataType.STRING(10),
-    allowNull: false
+    type: DataType.INTEGER,
+    allowNull: false,
+    field: 'engine_rpm',
+    comment: 'Revolutions per minute'
   })
-  unit!: string;
+  engine_rpm!: number;
 
   @Column({
-    type: DataType.DECIMAL(12,4),
-    allowNull: false
+    type: DataType.INTEGER,
+    allowNull: false,
+    field: 'shaft_power',
+    comment: 'Power in kilowatts'
   })
-  value!: number;
+  shaft_power!: number;
+
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+    field: 'fuel_oil_consumption',
+    comment: 'Consumption in kg/hour'
+  })
+  fuel_oil_consumption!: number;
+
+  @Column({
+    type: DataType.DECIMAL(3,1),
+    allowNull: false,
+    field: 'cylinder_lubricant_rate',
+    comment: 'Rate in grams per kWh'
+  })
+  cylinder_lubricant_rate!: number;
+
+  @Column({
+    type: DataType.DECIMAL(3,1),
+    allowNull: false,
+    field: 'scavenge_air_pressure',
+    comment: 'Pressure in bar'
+  })
+  scavenge_air_pressure!: number;
+
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+    field: 'turbocharger_rpm',
+    comment: 'Revolutions per minute'
+  })
+  turbocharger_rpm!: number;
 }
 
 export { OperatingConditions };
