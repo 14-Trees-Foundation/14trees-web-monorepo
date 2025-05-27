5a96d068d29bf44f083761c5e6b74ca677ce3508
elishaa31 <elishaaa31@gmail.com>
2025-05-27 17:43:22 +0530

    Added hover icon for PAN

--- apps/front-page/app/gift-trees/page.tsx
+++ apps/front-page/app/gift-trees/page.tsx
@@ -1358,7 +1358,12 @@ export default function GiftTreesPage() {
                       </div>
 
                       <div className="flex items-center flex-wrap">
-                        <label className="w-48 text-gray-700">PAN number*:</label>
+                        <label className="w-48 text-gray-700">
+                          PAN number*:
+                          <Tooltip title="PAN number is required for us to map and issue 80G receipt to the donor.">
+                            <InfoOutlinedIcon fontSize="small" className="text-gray-500 cursor-help" />
+                          </Tooltip>
+                          </label>
                         <div className="min-w-[200px] flex-1">
                           <input
                             type="text"
