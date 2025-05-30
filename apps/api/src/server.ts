7c1b086f1fd3773ad15d68deff90e73be8f93fd1
Dhruvin-Moradiya <dsmoradiya22@gmail.com>
2025-05-30 10:06:44 +0530

    Create campaigns

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -44,6 +44,7 @@ import emailTemplateRoutes from "./routes/emailTemplateRoutes";
 import paymentRoutes from "./routes/paymentRoutes";
 import whatsAppRoutes from "./routes/whatsAppRoutes";
 import viewRoutes from "./routes/viewRoutes";
+import campaignRoutes from "./routes/campaignRoutes";
 import swaggerJSDoc from "swagger-jsdoc";
 
 interface ResponseError extends Error {
@@ -129,6 +130,7 @@ const initExpressApp = (app: express.Application) => {
   app.use("/api/payments", paymentRoutes );
   app.use("/api/view", viewRoutes);
   app.use("/api/wa", whatsAppRoutes);
+  app.use("/api/campaigns", campaignRoutes);
 
   app.use('/api/doc', swaggerUi.serve, swaggerSpecification())
 
