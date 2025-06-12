52fda90e9b60ee6caacbb141d5d3a6f335304b1a
elishaa31 <elishaaa31@gmail.com>
2025-06-12 17:05:25 +0530

    Email Status update for Assignee

--- apps/api/src/models/gift_request_user.ts
+++ apps/api/src/models/gift_request_user.ts
@@ -10,12 +10,14 @@ interface GiftRequestUserAttributes {
     profile_image_url: string | null;
     mail_sent: boolean | null;
     mail_error: string | null;
+    mail_sent_assignee: boolean | null; 
+    mail_error_assignee: string | null;
     created_at: Date;
     updated_at: Date;
 }
 
 interface GiftRequestUserCreationAttributes
-    extends Optional<GiftRequestUserAttributes, 'id' | 'profile_image_url' | 'mail_error' | 'mail_sent' > { }
+    extends Optional<GiftRequestUserAttributes, 'id' | 'profile_image_url' | 'mail_error' | 'mail_sent' | 'mail_sent_assignee' | 'mail_error_assignee'> { }
 
 @Table({ tableName: 'gift_request_users' })
 class GiftRequestUser extends Model<GiftRequestUserAttributes, GiftRequestUserCreationAttributes>
@@ -66,6 +68,18 @@ class GiftRequestUser extends Model<GiftRequestUserAttributes, GiftRequestUserCr
         type: DataType.STRING,
     })
     mail_error!: string;
+
+    @Column({
+        type: DataType.BOOLEAN,
+        allowNull: true,
+    })
+    mail_sent_assignee!: boolean | null;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: true,
+    })
+    mail_error_assignee!: string | null;
 
     @Column({ type: DataType.DATE, allowNull: false })
     created_at!: Date;
