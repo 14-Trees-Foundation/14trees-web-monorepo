1551811a353bdeec942416dc35d3968cd0278b10
Vivek Jain <vivayush@gmail.com>
2025-06-30 17:27:26 +0530

    removing other non-required files and projects

--- apps/dashboard-next/app/profile/[saplingId]/page.tsx
+++
@@ -1,5 +0,0 @@
-// This is the profile page for a sapling (saplingId is a nextjs route parameter)
-export default function Profile({params}: { params: {saplingId: string}}) {
-    // get the saplingId from the route (nextjs app router)
-    return <div className="p-12 border border-white mx-auto w-full text-green-200">hello {params.saplingId}</div>
-}
