611cea7943159950d3f3661fa90edff4bcd158b2
Vivek Jain <vivayush@gmail.com>
2025-08-26 16:32:48 +0530

    name message

--- apps/api/src/controllers/helper/giftRequestHelper.ts
+++ apps/api/src/controllers/helper/giftRequestHelper.ts
@@ -27,8 +27,8 @@ import { ReferralsRepository } from "../../repo/referralsRepo";
 import { CampaignsRepository } from "../../repo/campaignsRepo";
 
 export const defaultGiftMessages = {
-    primary: 'Dear {recipient},\n\nWe are immensely delighted to share that a tree has been planted in your name at the 14 Trees Foundation, Pune. This tree will be nurtured in your honour, rejuvenating ecosystems, supporting biodiversity, and helping offset the harmful effects of climate change.',
-    birthday: 'Dear {recipient},\n\nWe are immensely delighted to share that a tree has been planted in your name on the occasion of your birthday at the 14 Trees Foundation, Pune. This tree will be nurtured in your honour, helping offset the harmful effects of climate change.',
+    primary: 'Dear {recipient},\n\nWe are immensely delighted to share that a tree has been planted in your name at the 14 Trees Foundation, Pune. This tree will be nurtured in your honour, rejuvenating ecosystems, supporting biodiversity, and helping offset the harmful effects of climate change.\n\nWe invite you to visit 14 Trees and firsthand experience the growth and contribution of your tree towards a greener future.',
+    birthday: 'Dear {recipient},\n\nWe are immensely delighted to share that a tree has been planted in your name on the occasion of your birthday at the 14 Trees Foundation, Pune. This tree will be nurtured in your honour, helping offset the harmful effects of climate change.\n\nWe invite you to visit 14 Trees and firsthand experience the growth and contribution of your tree towards a greener future.',
     memorial: 'A tree has been planted in the memory of <name here> at the 14 Trees Foundation reforestation site. For many years, this tree will help rejuvenate local ecosystems, support local biodiversity and offset the harmful effects of climate change and global warming.',
     secondary: 'We invite you to visit 14 Trees and firsthand experience the growth and contribution of your tree towards a greener future.',
     logo: 'Gifted by 14 Trees in partnership with'
